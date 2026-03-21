48999acc9a5eff067897faead45e7d98650bc7c7
240086 <2686251536@qq.com>
2026-03-21 13:00:53 +0800

    解耦

--- include/network/protocol/MessageMacro.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "network/protocol/MessageRegistry.h"
-
-// 注册 protobuf 消息
-#define REGISTER_PROTO_MESSAGE(MSG_ID, PB_TYPE) \
-    MessageRegistry::RegisterProto<PB_TYPE>(MSG_ID)
